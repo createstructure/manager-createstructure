45b82f945bf6d069b0afa0886de3d5b90def9c53
DavideC03 <help@castellanidavide.it>
2021-07-18 14:27:58 +0200

    v09.01.17

--- bin/manager.cpp
+++ bin/manager.cpp
@@ -1,7 +1,10 @@
 // Dependencies
 #include <bits/stdc++.h>
 #include "libraries/bin/json.hpp"
-#include "libraries/bin/createstructure.hpp"
+//#include "libraries/bin/createstructure.hpp"
+#include "libraries/bin/createstructure_memory.hpp"
+#include "libraries/bin/createstructure_work.hpp"
+#include "libraries/bin/createstructure_rest.hpp"
 
 // using ...
 using namespace std;
@@ -13,15 +16,16 @@ using json = nlohmann::json;
 //#define ONCE
 
 #ifdef DEBUG
-	#define VIEW_OUTPUT
+#define VIEW_OUTPUT
 #endif // DEBUG
 
 // Declared functions
 void eraseAllSubStr(string &mainStr, const string toErase);
 int main(int argc, char *argv[]);
 
-void eraseAllSubStr(string &mainStr, const string toErase) {
-        /* Decode Work: get decoded work instructions
+void eraseAllSubStr(string &mainStr, const string toErase)
+{
+	/* Decode Work: get decoded work instructions
          *
          * output:
          *      - decoded work instructions
@@ -29,13 +33,14 @@ void eraseAllSubStr(string &mainStr, const string toErase) {
          * output:
          *      - decoded work instructions
          */
-    size_t pos = string::npos;
-    while ((pos = mainStr.find(toErase) )!= string::npos)
-        mainStr.erase(pos, toErase.length());
+	size_t pos = string::npos;
+	while ((pos = mainStr.find(toErase)) != string::npos)
+		mainStr.erase(pos, toErase.length());
 }
 
 // Code
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	/* Main: the start point of the code
 	 *
 	 * inputs:
@@ -51,7 +56,8 @@ int main(int argc, char *argv[]) {
 	int i = 0;
 
 #ifndef ONCE
-	while (true) {
+	while (true)
+	{
 #endif // ONCE
 
 		waitUntilOkMemory();
@@ -60,30 +66,30 @@ int main(int argc, char *argv[]) {
 		json jData = getWork();
 		string data = jData["data"].dump();
 #ifdef DEFAULT
-	cout << data << endl;
+		cout << data << endl;
 #endif // DEFAULT
 		eraseAllSubStr(data, "\\u0000");
 
 		// Run job
 		system(
-				(
-					string("docker run ghcr.io/createstructure/core-createstructure '") +
-					data +
+			(
+				string("docker run ghcr.io/createstructure/core-createstructure '") +
+				data +
 #ifdef VIEW_OUTPUT
-					"' &"
-#else // VIEW_OUTPUT
-					"' > /dev/null &"
+				"' &"
+#else  // VIEW_OUTPUT
+			"' > /dev/null &"
 #endif // VIEW_OUTPUT
-				).c_str()
-			);
+				)
+				.c_str());
 		cout << "Runned process n°" << i++ << endl;
-		
+
 		// Set work as finished
 		json finishJson;
 		finishJson["server_id"] = jData["data"]["server_id"].get<string>();
 		finishJson["server_code"] = jData["data"]["server_code"].get<string>();
 		finishJson["work_id"] = jData["data"]["work_id"].get<string>();
-			string link("https:\u002F\u002Fwww.castellanidavide.it/other/rest/product/started_work.php");
+		string link("https:\u002F\u002Fwww.castellanidavide.it/other/rest/product/started_work.php");
 		request(link, "", finishJson, "POST");
 		sleep_for(500ms);
 #ifndef ONCE
